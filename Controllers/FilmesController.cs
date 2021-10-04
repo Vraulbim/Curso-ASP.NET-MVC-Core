73bcaebf7a26efcb53528576c46cd6f300d7b33f
Vitor Raul Bim <vitorrb36@gmail.com>
2021-10-04 15:40:54 -0300

    Aula de criação de formulários

---
+++ Controllers/FilmesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaDemoMVC.Controllers
+{
+    [Route("Filme")]
+    public class FilmesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Adicionar()
+        {
+            return View();
+        }
+    }
+}
